4e0354dd148d3ccfe5d182a89a3d91d11a67eef2
ruhiel_murrue <ruhiel1983@gmail.com>
2016-11-12 23:59:40 +0900

    初回コミット

--- GrayScale8bit/Program.cs
+++ GrayScale8bit/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GrayScale8bit
 {
@@ -10,6 +9,24 @@ namespace GrayScale8bit
 	{
 		static void Main(string[] args)
 		{
+			var path = args[0];
+
+			File.Move(path, path + ".bak");
+
+			var uri = new Uri(path + ".bak", UriKind.Relative);
+
+			// load
+			var bitmap = new BitmapImage(uri);
+
+			var formatConvertedBitmap = new FormatConvertedBitmap(bitmap,
+				PixelFormats.Gray8, null, 0);
+
+			using (var fs = new FileStream(path, FileMode.Create))
+			{
+				BitmapEncoder encoder = new BmpBitmapEncoder();
+				encoder.Frames.Add(BitmapFrame.Create(formatConvertedBitmap));
+				encoder.Save(fs);
+			}
 		}
 	}
 }
